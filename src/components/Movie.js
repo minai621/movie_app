a2a1ceb40f6b92282fdc0624d83fa025b09f8ad2
minje Kong <minai621@naver.com>
2021-06-17 12:52:37 +0900

    21-06-17

---
+++ src/components/Movie.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Movie.css';
+import {Link} from 'react-router-dom';
+
+function Movie({title, year, summary, poster, genres}){
+    return (
+        <div className="movie">
+        <Link to={{pathname: '/movie-detail', state: {year, title, summary, poster, genres}}}>
+            <img src={poster} alt={title} title={title} />
+                <h3 className="movie_title">{title}</h3>
+                <h5 className="movie_year">{year}</h5>
+                <p className="movie_summary">{summary.slice(0, 180)}...</p>
+                <ul className="movie_genres">
+                    {genres.map((genre, index) => {
+                        return <li key={index} className="movie_genre">{genre}</li>;
+                    })}
+                </ul>
+            </Link>
+        </div>
+    )
+}
+
+Movie.propTypes ={
+    year: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    summary: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired,
+    genres: PropTypes.string.isRequired,
+};
+
+export default Movie;
